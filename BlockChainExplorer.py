5f681e006e37570773e0cbb605a313e025d70e40
ManicalEngineer <36464331+ManicalEngineer@users.noreply.github.com>
2023-05-30 21:12:22 -0400

    Update getBalance and add getContractCreatation

--- BlockChainExplorer.py
+++ BlockChainExplorer.py
@@ -4,7 +4,7 @@ import json
 
 
 class Explorer:
-
+	#requires chain id - supports avax, bsc, polygon & eth, also requires api key for the appropriate blockchain explorer
 	def __init__(self, chain, api_key):
 	    self.api_key = api_key
 	    chain_dict = {'avax' : 'https://api.snowtrace.io/api',
@@ -19,12 +19,14 @@ class Explorer:
 		dict_str = byte_str.decode("UTF-8")
 		return eval(dict_str)
 
+	#takes contract address and returns the json of the ABI
 	def getABI(self, address):
 		request_url = f"{self.chain}?module=contract&action=getabi&address={address}&apikey={self.api_key}"
 		response = requests.get(request_url)
 		abi = json.loads(self.convResponse(response)['result'])
 		return abi
 		
+	#takes contract address and returns the source code	
 	def getSource(self, address):
 		request_url = f"{self.chain}?module=contract&action=getsourcecode&address={address}&apikey={self.api_key}"
 		response = requests.get(request_url).json()
@@ -34,6 +36,8 @@ class Explorer:
 			print(response)
 			return None
 
+
+	#takes contract_address or wallet address note - one needs to be set to None. Returns all the token transacations
 	def getTransactions(self, contract_address, address, start_block = 0, end_block = 99999999):
 		request_url = f"{self.chain}?module=account&action=tokentx"
 		if contract_address is not None:
@@ -47,6 +51,7 @@ class Explorer:
 		txs = self.convResponse(response)
 		return txs['result']
 
+	#takes contract_address or wallet address note - one needs to be set to None. Returns all the 'normal' transacations
 	def getTransactionList(self, contract_address, address, start_block = 0, end_block = 99999999):
 		request_url = f"{self.chain}?module=account&action=txlist"
 		if contract_address is not None:
@@ -60,6 +65,7 @@ class Explorer:
 		txs = self.convResponse(response)
 		return txs['result']
 
+	#takes contract_address or wallet address note - one needs to be set to None. Returns all the 'internal' transacations
 	def getInternalTransactionList(self, contract_address, address, start_block = 0, end_block = 99999999):
 		request_url = f"{self.chain}?module=account&action=txlistinternal"
 		if contract_address is not None:
@@ -73,16 +79,20 @@ class Explorer:
 		txs = self.convResponse(response)
 		return txs['result']
 
-
+	#takes a unix timestamp and turns the block number for that timestamp
 	def getBlockByTimestamp(self, timestamp):
 		request_url = f"{self.chain}?module=block&action=getblocknobytime&timestamp={timestamp}&closest=before&apikey={self.api_key}"
 		response = requests.get(request_url).json()
 		return int(response['result'])
 
+	#takes a single address or a list of upto 20 addresses and returns the native token balance(s)
 	def getBalance(self, address):
 		if isinstance(address, list):
 			if not (len(address) > 20):
-				request_url = f"{self.chain}?module=account&action=balancemulti&address={address}&tag=latest&apikey={self.api_key}"
+				addressStr = address[0]
+				for addy in address:
+					addressStr += ', ' + addy
+				request_url = f"{self.chain}?module=account&action=balancemulti&address={addressStr}&tag=latest&apikey={self.api_key}"
 				response = requests.get(request_url).json()
 				return response['result']
 		else:
@@ -91,3 +101,20 @@ class Explorer:
 			return response['result']
 
 		return -1
+
+	#takes a single contract address or a list of upto 5 contract addresses and returns the creator wallet and transaction hash
+	def getContractCreation(self, address):
+		if isinstance(address, list):
+			if not (len(address) > 5):
+				addressStr = address[0]
+				for addy in address:
+					addressStr += ', ' + addy
+				request_url = f"{self.chain}?module=contract&action=getcontractcreation&address={addressStr}&tag=latest&apikey={self.api_key}"
+				response = requests.get(request_url).json()
+				return response['result']
+		else:
+			request_url = f"{self.chain}?module=contract&action=getcontractcreation&address={address}&tag=latest&apikey={self.api_key}"
+			response = requests.get(request_url).json()
+			return response['result']
+
+		return -1		
