45afbe45f9d03b5a0ffa3bde13213d73a768180e
ManicalEngineer <36464331+ManicalEngineer@users.noreply.github.com>
2023-06-27 22:25:16 -0400

    Added Doc Strings to source

--- src/BlockChainExplorer/BlockChainExplorer.py
+++ src/BlockChainExplorer/BlockChainExplorer.py
@@ -4,30 +4,34 @@ import json
 
 
 class Explorer:
-	#requires chain id - supports avax, bsc, polygon & eth, also requires api key for the appropriate blockchain explorer
-	def __init__(self, chain, api_key):
+	"""requires chain id - supports avax, bsc, polygon & eth, also requires api key for the appropriate blockchain explorer"""
+	def __init__(self, chain: str, api_key: str):
 	    self.api_key = api_key
+	    """Your api key for the blockchain explorer you're trying to access"""
 	    chain_dict = {'avax' : 'https://api.snowtrace.io/api',
 	    				   'bsc' : 'https://api.bscscan.com/api',
 	    				   'polygon' : 'https://api.polygonscan.com/api',
 	    				   'eth' : 'https://api.etherscan.io/api' }
 
 	    self.chain = chain_dict[chain]
+	    """ 'eth', 'bsc', 'polygon' or 'avax' for the chain you're trying to access"""
 
 	def convResponse(self, data):
 		byte_str = data.content
 		dict_str = byte_str.decode("UTF-8")
 		return eval(dict_str)
 
-	#takes contract address and returns the json of the ABI
-	def getABI(self, address):
+	
+	def getABI(self, address: str):
+		"""takes contract address as a string and returns the json of the ABI"""
 		request_url = f"{self.chain}?module=contract&action=getabi&address={address}&apikey={self.api_key}"
 		response = requests.get(request_url)
 		abi = json.loads(self.convResponse(response)['result'])
 		return abi
 		
-	#takes contract address and returns the source code	
-	def getSource(self, address):
+	
+	def getSource(self, address: str):
+		"""takes contract address as a string and returns the source code"""
 		request_url = f"{self.chain}?module=contract&action=getsourcecode&address={address}&apikey={self.api_key}"
 		response = requests.get(request_url).json()
 		if response['message'] == "OK":
@@ -37,8 +41,9 @@ class Explorer:
 			return None
 
 
-	#takes contract_address or wallet address note - one needs to be set to None. Returns all the token transacations
-	def getTransactions(self, contract_address, address, start_block = 0, end_block = 99999999):
+	
+	def getTransactions(self, contract_address: str, address: str, start_block: int = 0, end_block: int = 99999999):
+		"""takes contract_address or wallet address as a string note - one needs to be set to None. Returns all the token transacations as a dict"""
 		request_url = f"{self.chain}?module=account&action=tokentx"
 		if contract_address is not None:
 			request_url += f"&contractaddress={contract_address}"
@@ -51,8 +56,9 @@ class Explorer:
 		txs = self.convResponse(response)
 		return txs['result']
 
-	#takes contract_address or wallet address note - one needs to be set to None. Returns all the 'normal' transacations
-	def getTransactionList(self, contract_address, address, start_block = 0, end_block = 99999999):
+	
+	def getTransactionList(self, contract_address: str, address: str, start_block: int = 0, end_block: int = 99999999):
+		"""takes contract_address or wallet address as a string note - one needs to be set to None. Returns all the 'normal' transacations as a list of dicts"""
 		request_url = f"{self.chain}?module=account&action=txlist"
 		if contract_address is not None:
 			request_url += f"&contractaddress={contract_address}"
@@ -61,12 +67,12 @@ class Explorer:
 		request_url += f"&startblock={start_block}&endblock={end_block}&sort=asc&apikey={self.api_key}"
 
 		response = requests.get(request_url)
-		#print(response.content)
 		txs = self.convResponse(response)
 		return txs['result']
 
-	#takes contract_address or wallet address note - one needs to be set to None. Returns all the 'internal' transacations
-	def getInternalTransactionList(self, contract_address, address, start_block = 0, end_block = 99999999):
+	
+	def getInternalTransactionList(self, contract_address: str, address: str, start_block: int = 0, end_block: int = 99999999):
+		"""takes contract_address or wallet address as a string note - one needs to be set to None. Returns all the 'internal' transacations as a list of dicts"""
 		request_url = f"{self.chain}?module=account&action=txlistinternal"
 		if contract_address is not None:
 			request_url += f"&contractaddress={contract_address}"
@@ -75,18 +81,19 @@ class Explorer:
 		request_url += f"&startblock={start_block}&endblock={end_block}&sort=asc&apikey={self.api_key}"
 
 		response = requests.get(request_url)
-		#print(response.content)
 		txs = self.convResponse(response)
 		return txs['result']
 
-	#takes a unix timestamp and turns the block number for that timestamp
-	def getBlockByTimestamp(self, timestamp):
+	
+	def getBlockByTimestamp(self, timestamp: int):
+		"""takes a unix timestamp as an int and turns the block number for that timestamp"""
 		request_url = f"{self.chain}?module=block&action=getblocknobytime&timestamp={timestamp}&closest=before&apikey={self.api_key}"
 		response = requests.get(request_url).json()
 		return int(response['result'])
 
-	#takes a single address or a list of upto 20 addresses and returns the native token balance(s)
-	def getBalance(self, address):
+	
+	def getBalance(self, address: str):
+		"""takes a single address as a string or a list of upto 20 addresses and returns the native token balance(s)"""
 		if isinstance(address, list):
 			if not (len(address) > 20):
 				addressStr = address[0]
@@ -102,8 +109,9 @@ class Explorer:
 
 		return -1
 
-	#takes a single contract address or a list of upto 5 contract addresses and returns the creator wallet and transaction hash
-	def getContractCreation(self, address):
+	
+	def getContractCreation(self, address: str):
+		"""takes a single contract address as a string or a list of upto 5 contract addresses and returns the creator wallet and transaction hash as a list of dicts"""
 		if isinstance(address, list):
 			if not (len(address) > 5):
 				addressStr = address[0]
@@ -119,7 +127,9 @@ class Explorer:
 
 		return -1		
 
-	def getInternalTransactionsByTx(tx):
+	
+	def getInternalTransactionsByTx(self, tx: str):
+		"""Takes a transaction hash as a string and returns the internal transactions as a list of dicts"""
 		request_url = f'{self.chain}?module=account&action=txlistinternal&txhash={tx}&apikey={self.api_key}'
 		response = requests.get(request_url).json()
 		return response
